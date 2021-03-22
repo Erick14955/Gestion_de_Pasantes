937071afab2793ca5bbacb4a88c596e46ae9816e
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-03-22 18:28:51 -0400

    Agregando imagen al boton buscar

--- UI/Registros/rPasantes.Designer.cs
+++ UI/Registros/rPasantes.Designer.cs
@@ -30,6 +30,7 @@ namespace Gestion_de_Pasantes.UI.Registros
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(rPasantes));
             this.label1 = new System.Windows.Forms.Label();
             this.UsuarioIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
             this.AliasTextBox = new System.Windows.Forms.TextBox();
@@ -61,122 +62,129 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Location = new System.Drawing.Point(10, 26);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.Size = new System.Drawing.Size(61, 15);
             this.label1.TabIndex = 0;
             this.label1.Text = "Pasante Id";
             // 
             // UsuarioIdNumericUpDown
             // 
-            this.UsuarioIdNumericUpDown.Location = new System.Drawing.Point(134, 35);
+            this.UsuarioIdNumericUpDown.Location = new System.Drawing.Point(117, 26);
+            this.UsuarioIdNumericUpDown.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.UsuarioIdNumericUpDown.Name = "UsuarioIdNumericUpDown";
-            this.UsuarioIdNumericUpDown.Size = new System.Drawing.Size(150, 27);
+            this.UsuarioIdNumericUpDown.Size = new System.Drawing.Size(131, 23);
             this.UsuarioIdNumericUpDown.TabIndex = 1;
             // 
             // AliasTextBox
             // 
-            this.AliasTextBox.Location = new System.Drawing.Point(134, 95);
+            this.AliasTextBox.Location = new System.Drawing.Point(117, 71);
+            this.AliasTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.AliasTextBox.Name = "AliasTextBox";
-            this.AliasTextBox.Size = new System.Drawing.Size(383, 27);
+            this.AliasTextBox.Size = new System.Drawing.Size(336, 23);
             this.AliasTextBox.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Location = new System.Drawing.Point(10, 71);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.Size = new System.Drawing.Size(76, 15);
             this.label2.TabIndex = 3;
             this.label2.Text = "Institucion Id";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Location = new System.Drawing.Point(10, 117);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.Size = new System.Drawing.Size(56, 15);
             this.label3.TabIndex = 5;
             this.label3.Text = "Nombres";
             // 
             // NombreTextBox
             // 
-            this.NombreTextBox.Location = new System.Drawing.Point(134, 156);
+            this.NombreTextBox.Location = new System.Drawing.Point(117, 117);
+            this.NombreTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NombreTextBox.Name = "NombreTextBox";
-            this.NombreTextBox.Size = new System.Drawing.Size(383, 27);
+            this.NombreTextBox.Size = new System.Drawing.Size(336, 23);
             this.NombreTextBox.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 217);
+            this.label4.Location = new System.Drawing.Point(10, 163);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.Size = new System.Drawing.Size(56, 15);
             this.label4.TabIndex = 7;
             this.label4.Text = "Apellidos";
             // 
             // ClaveTextBox
             // 
-            this.ClaveTextBox.Location = new System.Drawing.Point(134, 217);
+            this.ClaveTextBox.Location = new System.Drawing.Point(117, 163);
+            this.ClaveTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ClaveTextBox.Name = "ClaveTextBox";
-            this.ClaveTextBox.Size = new System.Drawing.Size(383, 27);
+            this.ClaveTextBox.Size = new System.Drawing.Size(336, 23);
             this.ClaveTextBox.TabIndex = 6;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 284);
+            this.label5.Location = new System.Drawing.Point(10, 213);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.Size = new System.Drawing.Size(57, 15);
             this.label5.TabIndex = 9;
             this.label5.Text = "Matricula";
             // 
             // ConfirmarClaveTextBox
             // 
-            this.ConfirmarClaveTextBox.Location = new System.Drawing.Point(134, 277);
+            this.ConfirmarClaveTextBox.Location = new System.Drawing.Point(117, 208);
+            this.ConfirmarClaveTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ConfirmarClaveTextBox.Name = "ConfirmarClaveTextBox";
-            this.ConfirmarClaveTextBox.Size = new System.Drawing.Size(383, 27);
+            this.ConfirmarClaveTextBox.Size = new System.Drawing.Size(336, 23);
             this.ConfirmarClaveTextBox.TabIndex = 8;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(575, 220);
+            this.label6.Location = new System.Drawing.Point(503, 165);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.Size = new System.Drawing.Size(36, 15);
             this.label6.TabIndex = 11;
             this.label6.Text = "Email";
             // 
             // EmailTextBox
             // 
-            this.EmailTextBox.Location = new System.Drawing.Point(688, 217);
+            this.EmailTextBox.Location = new System.Drawing.Point(602, 163);
+            this.EmailTextBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.EmailTextBox.Name = "EmailTextBox";
-            this.EmailTextBox.Size = new System.Drawing.Size(362, 27);
+            this.EmailTextBox.Size = new System.Drawing.Size(317, 23);
             this.EmailTextBox.TabIndex = 10;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(575, 92);
+            this.label7.Location = new System.Drawing.Point(503, 69);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(75, 20);
+            this.label7.Size = new System.Drawing.Size(58, 15);
             this.label7.TabIndex = 12;
             this.label7.Text = "Habilidad";
             // 
             // RolComboBox
             // 
             this.RolComboBox.FormattingEnabled = true;
-            this.RolComboBox.Location = new System.Drawing.Point(688, 89);
+            this.RolComboBox.Location = new System.Drawing.Point(602, 67);
+            this.RolComboBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.RolComboBox.Name = "RolComboBox";
-            this.RolComboBox.Size = new System.Drawing.Size(362, 28);
+            this.RolComboBox.Size = new System.Drawing.Size(317, 23);
             this.RolComboBox.TabIndex = 13;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(575, 280);
+            this.label8.Location = new System.Drawing.Point(503, 210);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(109, 20);
+            this.label8.Size = new System.Drawing.Size(88, 15);
             this.label8.TabIndex = 14;
             this.label8.Text = "Fecha Creacion";
             // 
@@ -184,53 +192,61 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             this.FechaCreacionDateTimePicker.CustomFormat = "dd / MM / yyyy";
             this.FechaCreacionDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.FechaCreacionDateTimePicker.Location = new System.Drawing.Point(688, 275);
+            this.FechaCreacionDateTimePicker.Location = new System.Drawing.Point(602, 206);
+            this.FechaCreacionDateTimePicker.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.FechaCreacionDateTimePicker.Name = "FechaCreacionDateTimePicker";
-            this.FechaCreacionDateTimePicker.Size = new System.Drawing.Size(362, 27);
+            this.FechaCreacionDateTimePicker.Size = new System.Drawing.Size(317, 23);
             this.FechaCreacionDateTimePicker.TabIndex = 15;
             // 
             // ActivoCheckBox
             // 
             this.ActivoCheckBox.AutoSize = true;
-            this.ActivoCheckBox.Location = new System.Drawing.Point(977, 38);
+            this.ActivoCheckBox.Location = new System.Drawing.Point(855, 28);
+            this.ActivoCheckBox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.ActivoCheckBox.Name = "ActivoCheckBox";
-            this.ActivoCheckBox.Size = new System.Drawing.Size(73, 24);
+            this.ActivoCheckBox.Size = new System.Drawing.Size(60, 19);
             this.ActivoCheckBox.TabIndex = 16;
             this.ActivoCheckBox.Text = "Activo";
             this.ActivoCheckBox.UseVisualStyleBackColor = true;
             // 
             // BuscarButton
             // 
-            this.BuscarButton.Location = new System.Drawing.Point(304, 33);
+            this.BuscarButton.Image = ((System.Drawing.Image)(resources.GetObject("BuscarButton.Image")));
+            this.BuscarButton.Location = new System.Drawing.Point(266, 25);
+            this.BuscarButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.BuscarButton.Name = "BuscarButton";
-            this.BuscarButton.Size = new System.Drawing.Size(95, 29);
+            this.BuscarButton.Size = new System.Drawing.Size(83, 24);
             this.BuscarButton.TabIndex = 17;
-            this.BuscarButton.Text = "a";
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
             this.BuscarButton.UseVisualStyleBackColor = true;
             // 
             // NuevoButton
             // 
-            this.NuevoButton.Location = new System.Drawing.Point(284, 345);
+            this.NuevoButton.Location = new System.Drawing.Point(248, 259);
+            this.NuevoButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.NuevoButton.Name = "NuevoButton";
-            this.NuevoButton.Size = new System.Drawing.Size(115, 45);
+            this.NuevoButton.Size = new System.Drawing.Size(101, 34);
             this.NuevoButton.TabIndex = 18;
             this.NuevoButton.Text = "Nuevo";
             this.NuevoButton.UseVisualStyleBackColor = true;
             // 
             // GuardarButton
             // 
-            this.GuardarButton.Location = new System.Drawing.Point(462, 345);
+            this.GuardarButton.Location = new System.Drawing.Point(404, 259);
+            this.GuardarButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.GuardarButton.Name = "GuardarButton";
-            this.GuardarButton.Size = new System.Drawing.Size(115, 45);
+            this.GuardarButton.Size = new System.Drawing.Size(101, 34);
             this.GuardarButton.TabIndex = 19;
             this.GuardarButton.Text = "Guardar";
             this.GuardarButton.UseVisualStyleBackColor = true;
             // 
             // EliminarButton
             // 
-            this.EliminarButton.Location = new System.Drawing.Point(643, 345);
+            this.EliminarButton.Location = new System.Drawing.Point(563, 259);
+            this.EliminarButton.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.EliminarButton.Name = "EliminarButton";
-            this.EliminarButton.Size = new System.Drawing.Size(115, 45);
+            this.EliminarButton.Size = new System.Drawing.Size(101, 34);
             this.EliminarButton.TabIndex = 20;
             this.EliminarButton.Text = "Eliminar";
             this.EliminarButton.UseVisualStyleBackColor = true;
@@ -242,24 +258,25 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(575, 159);
+            this.label9.Location = new System.Drawing.Point(503, 119);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(85, 20);
+            this.label9.Size = new System.Drawing.Size(66, 15);
             this.label9.TabIndex = 22;
             this.label9.Text = "Total Horas";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(688, 156);
+            this.textBox1.Location = new System.Drawing.Point(602, 117);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(362, 27);
+            this.textBox1.Size = new System.Drawing.Size(317, 23);
             this.textBox1.TabIndex = 21;
             // 
             // rPasantes
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1075, 419);
+            this.ClientSize = new System.Drawing.Size(941, 314);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.EliminarButton);
@@ -283,6 +300,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             this.Controls.Add(this.AliasTextBox);
             this.Controls.Add(this.UsuarioIdNumericUpDown);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "rPasantes";
             this.Text = "Registro de Pasantes";
             ((System.ComponentModel.ISupportInitialize)(this.UsuarioIdNumericUpDown)).EndInit();
