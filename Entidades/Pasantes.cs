a06b2621297cb20358b96406ff5ad7e36e5dbb7d
Erickson Fana Morales <erickson_fana@ucne.edu.do>
2021-03-22 17:56:28 -0400

    Creando las variables de la entidad Pasantes

--- Entidades/Pasantes.cs
+++ Entidades/Pasantes.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,16 @@ namespace Gestion_de_Pasantes.Entidades
 {
     class Pasantes
     {
+        [Key]
+        public int id { get; set; }
+        public string NombrePasante { get; set; }
+        public string Institucion { get; set; }
+        public string ApellidosPasantes { get; set; }
+        public int Matricula { get; set; }
+        public string Habilidad { get; set; }
+        public int TotalHoras { get; set; }
+        public string Email { get; set; }
+        public bool Activo { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
     }
 }
