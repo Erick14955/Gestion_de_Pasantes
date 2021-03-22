6e40cc62520f2b20da4f2ff869a9784a5d0a8ab2
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-03-22 17:47:16 -0400

    Creando el registro de pasantes.

---
+++ UI/Registros/rPasantes.Designer.cs
@@ -0,0 +1,322 @@
+﻿
+namespace Gestion_de_Pasantes.UI.Registros
+{
+    partial class rPasantes
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.UsuarioIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.AliasTextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.NombreTextBox = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.ClaveTextBox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.ConfirmarClaveTextBox = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.RolComboBox = new System.Windows.Forms.ComboBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.FechaCreacionDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.ActivoCheckBox = new System.Windows.Forms.CheckBox();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.MyErrorProvider = new System.Windows.Forms.ErrorProvider(this.components);
+            this.label9 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.UsuarioIdNumericUpDown)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pasante Id";
+            // 
+            // UsuarioIdNumericUpDown
+            // 
+            this.UsuarioIdNumericUpDown.Location = new System.Drawing.Point(134, 35);
+            this.UsuarioIdNumericUpDown.Name = "UsuarioIdNumericUpDown";
+            this.UsuarioIdNumericUpDown.Size = new System.Drawing.Size(150, 27);
+            this.UsuarioIdNumericUpDown.TabIndex = 1;
+            // 
+            // AliasTextBox
+            // 
+            this.AliasTextBox.Location = new System.Drawing.Point(134, 95);
+            this.AliasTextBox.Name = "AliasTextBox";
+            this.AliasTextBox.Size = new System.Drawing.Size(383, 27);
+            this.AliasTextBox.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(94, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Institucion Id";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 156);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(70, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nombres";
+            // 
+            // NombreTextBox
+            // 
+            this.NombreTextBox.Location = new System.Drawing.Point(134, 156);
+            this.NombreTextBox.Name = "NombreTextBox";
+            this.NombreTextBox.Size = new System.Drawing.Size(383, 27);
+            this.NombreTextBox.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 217);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Apellidos";
+            // 
+            // ClaveTextBox
+            // 
+            this.ClaveTextBox.Location = new System.Drawing.Point(134, 217);
+            this.ClaveTextBox.Name = "ClaveTextBox";
+            this.ClaveTextBox.Size = new System.Drawing.Size(383, 27);
+            this.ClaveTextBox.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 284);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Matricula";
+            // 
+            // ConfirmarClaveTextBox
+            // 
+            this.ConfirmarClaveTextBox.Location = new System.Drawing.Point(134, 277);
+            this.ConfirmarClaveTextBox.Name = "ConfirmarClaveTextBox";
+            this.ConfirmarClaveTextBox.Size = new System.Drawing.Size(383, 27);
+            this.ConfirmarClaveTextBox.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(575, 220);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Email";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(688, 217);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(362, 27);
+            this.EmailTextBox.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(575, 92);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(75, 20);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Habilidad";
+            // 
+            // RolComboBox
+            // 
+            this.RolComboBox.FormattingEnabled = true;
+            this.RolComboBox.Location = new System.Drawing.Point(688, 89);
+            this.RolComboBox.Name = "RolComboBox";
+            this.RolComboBox.Size = new System.Drawing.Size(362, 28);
+            this.RolComboBox.TabIndex = 13;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(575, 280);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(109, 20);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "Fecha Creacion";
+            // 
+            // FechaCreacionDateTimePicker
+            // 
+            this.FechaCreacionDateTimePicker.CustomFormat = "dd / MM / yyyy";
+            this.FechaCreacionDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.FechaCreacionDateTimePicker.Location = new System.Drawing.Point(688, 275);
+            this.FechaCreacionDateTimePicker.Name = "FechaCreacionDateTimePicker";
+            this.FechaCreacionDateTimePicker.Size = new System.Drawing.Size(362, 27);
+            this.FechaCreacionDateTimePicker.TabIndex = 15;
+            // 
+            // ActivoCheckBox
+            // 
+            this.ActivoCheckBox.AutoSize = true;
+            this.ActivoCheckBox.Location = new System.Drawing.Point(977, 38);
+            this.ActivoCheckBox.Name = "ActivoCheckBox";
+            this.ActivoCheckBox.Size = new System.Drawing.Size(73, 24);
+            this.ActivoCheckBox.TabIndex = 16;
+            this.ActivoCheckBox.Text = "Activo";
+            this.ActivoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Location = new System.Drawing.Point(304, 33);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(95, 29);
+            this.BuscarButton.TabIndex = 17;
+            this.BuscarButton.Text = "a";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Location = new System.Drawing.Point(284, 345);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(115, 45);
+            this.NuevoButton.TabIndex = 18;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Location = new System.Drawing.Point(462, 345);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(115, 45);
+            this.GuardarButton.TabIndex = 19;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Location = new System.Drawing.Point(643, 345);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(115, 45);
+            this.EliminarButton.TabIndex = 20;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // MyErrorProvider
+            // 
+            this.MyErrorProvider.ContainerControl = this;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(575, 159);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(85, 20);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "Total Horas";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(688, 156);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(362, 27);
+            this.textBox1.TabIndex = 21;
+            // 
+            // rPasantes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1075, 419);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.ActivoCheckBox);
+            this.Controls.Add(this.FechaCreacionDateTimePicker);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.RolComboBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.ConfirmarClaveTextBox);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.ClaveTextBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.NombreTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.AliasTextBox);
+            this.Controls.Add(this.UsuarioIdNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "rPasantes";
+            this.Text = "Registro de Pasantes";
+            ((System.ComponentModel.ISupportInitialize)(this.UsuarioIdNumericUpDown)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.MyErrorProvider)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown UsuarioIdNumericUpDown;
+        private System.Windows.Forms.TextBox AliasTextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox NombreTextBox;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox ClaveTextBox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox ConfirmarClaveTextBox;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.ComboBox RolComboBox;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.DateTimePicker FechaCreacionDateTimePicker;
+        private System.Windows.Forms.CheckBox ActivoCheckBox;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.ErrorProvider MyErrorProvider;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
