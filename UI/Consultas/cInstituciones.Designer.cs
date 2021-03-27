4428daf8092bcfdef21bf3322e642f3cd7bf9ccc
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-03-27 12:35:31 +0800

    cIntituciones

---
+++ UI/Consultas/cInstituciones.Designer.cs
@@ -0,0 +1,254 @@
+﻿
+namespace Gestion_de_Pasantes.UI.Consultas
+{
+    partial class cInstituciones
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(cInstituciones));
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.FiltroTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePicker2 = new System.Windows.Forms.DateTimePicker();
+            this.InformacionTextBox = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.TodoRadioButton = new System.Windows.Forms.RadioButton();
+            this.ActivoRadioButton2 = new System.Windows.Forms.RadioButton();
+            this.InactivoRadioButton = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 18);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(126, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "Uso Filtro de Fecha";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Filtro";
+            // 
+            // FiltroTextBox
+            // 
+            this.FiltroTextBox.Location = new System.Drawing.Point(43, 51);
+            this.FiltroTextBox.Multiline = true;
+            this.FiltroTextBox.Name = "FiltroTextBox";
+            this.FiltroTextBox.Size = new System.Drawing.Size(108, 23);
+            this.FiltroTextBox.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(157, 1);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(309, 44);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(155, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 15);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Desde";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 15);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Hasta";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(49, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(100, 23);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // dateTimePicker2
+            // 
+            this.dateTimePicker2.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker2.Location = new System.Drawing.Point(199, 12);
+            this.dateTimePicker2.Name = "dateTimePicker2";
+            this.dateTimePicker2.Size = new System.Drawing.Size(97, 23);
+            this.dateTimePicker2.TabIndex = 5;
+            // 
+            // InformacionTextBox
+            // 
+            this.InformacionTextBox.Location = new System.Drawing.Point(157, 51);
+            this.InformacionTextBox.Name = "InformacionTextBox";
+            this.InformacionTextBox.Size = new System.Drawing.Size(309, 23);
+            this.InformacionTextBox.TabIndex = 4;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.InactivoRadioButton);
+            this.groupBox2.Controls.Add(this.ActivoRadioButton2);
+            this.groupBox2.Controls.Add(this.TodoRadioButton);
+            this.groupBox2.Location = new System.Drawing.Point(472, 1);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(81, 73);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "EsActivo?";
+            // 
+            // TodoRadioButton
+            // 
+            this.TodoRadioButton.AutoSize = true;
+            this.TodoRadioButton.Location = new System.Drawing.Point(6, 16);
+            this.TodoRadioButton.Name = "TodoRadioButton";
+            this.TodoRadioButton.Size = new System.Drawing.Size(56, 19);
+            this.TodoRadioButton.TabIndex = 0;
+            this.TodoRadioButton.TabStop = true;
+            this.TodoRadioButton.Text = "Todos";
+            this.TodoRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // ActivoRadioButton2
+            // 
+            this.ActivoRadioButton2.AutoSize = true;
+            this.ActivoRadioButton2.Location = new System.Drawing.Point(6, 33);
+            this.ActivoRadioButton2.Name = "ActivoRadioButton2";
+            this.ActivoRadioButton2.Size = new System.Drawing.Size(59, 19);
+            this.ActivoRadioButton2.TabIndex = 1;
+            this.ActivoRadioButton2.TabStop = true;
+            this.ActivoRadioButton2.Text = "Activo";
+            this.ActivoRadioButton2.UseVisualStyleBackColor = true;
+            // 
+            // InactivoRadioButton
+            // 
+            this.InactivoRadioButton.AutoSize = true;
+            this.InactivoRadioButton.Location = new System.Drawing.Point(6, 50);
+            this.InactivoRadioButton.Name = "InactivoRadioButton";
+            this.InactivoRadioButton.Size = new System.Drawing.Size(67, 19);
+            this.InactivoRadioButton.TabIndex = 2;
+            this.InactivoRadioButton.TabStop = true;
+            this.InactivoRadioButton.Text = "Inactivo";
+            this.InactivoRadioButton.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.button1.Location = new System.Drawing.Point(559, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 65);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Buscar";
+            this.button1.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 81);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(640, 226);
+            this.dataGridView1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 313);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(119, 26);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Imprimir";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // cInstituciones
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 341);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.InformacionTextBox);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.FiltroTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "cInstituciones";
+            this.Text = "cInstituciones";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox FiltroTextBox;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker2;
+        private System.Windows.Forms.TextBox InformacionTextBox;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton InactivoRadioButton;
+        private System.Windows.Forms.RadioButton ActivoRadioButton2;
+        private System.Windows.Forms.RadioButton TodoRadioButton;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+    }
+}
