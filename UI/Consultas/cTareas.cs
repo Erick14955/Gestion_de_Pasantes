bf48dfbd3b83ae3ec8af24b837ad3da12685b3c1
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-03-31 10:16:13 +0800

    Ventanas de Consultas Usuario, Tareas, Pasanyes  Roles

---
+++ UI/Consultas/cTareas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gestion_de_Pasantes.UI.Consultas
+{
+    public partial class cTareas : Form
+    {
+        public cTareas()
+        {
+            InitializeComponent();
+        }
+    }
+}
