44e22b491b4757c3a6cb70cc27a2235073c2e18f
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-03-30 21:53:06 -0400

    Agregando los atributos de la clase Usuarios.

--- Entidades/Usuarios.cs
+++ Entidades/Usuarios.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,26 @@ namespace Gestion_de_Pasantes.Entidades
 {
     public class Usuarios
     {
+        [Key]
+        public int UsuarioID { get; set; }
+        public string NombreUsuario { get; set; }
+        public string AliasUsuario { get; set; }
+        public string Email { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaUsuario { get; set; }
+        public string Rol { get; set; }
+        public bool Activo { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioID = 0;
+            NombreUsuario = string.Empty;
+            AliasUsuario = string.Empty;
+            Email = string.Empty;
+            Clave = string.Empty;
+            FechaUsuario = DateTime.Now;
+            Rol = string.Empty;
+            Activo = false;
+        }
     }
 }
