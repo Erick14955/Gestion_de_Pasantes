c06c1be68811d176c9a7a3fe31833637c87c8454
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-03-31 23:11:54 -0400

    Agregando el nuevo registro de pasantes.

---
+++ UI/Registros/rPasantes.Designer.cs
@@ -0,0 +1,391 @@
+﻿
+namespace Gestion_de_Pasantes.UI.Registros
+{
+    partial class rPasantes
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.PasanteIdNumericUpDown = new System.Windows.Forms.NumericUpDown();
+            this.BuscarButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.FechaCreacionDateTimePicker = new System.Windows.Forms.DateTimePicker();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.EmailTextBox = new System.Windows.Forms.TextBox();
+            this.InstitucionComboBox = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.MatriculaTextBox = new System.Windows.Forms.TextBox();
+            this.NombrePasanteTextBox = new System.Windows.Forms.TextBox();
+            this.TelefonoTextBox = new System.Windows.Forms.TextBox();
+            this.HorasARealizarTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RemoverButton = new System.Windows.Forms.Button();
+            this.AgregarButton = new System.Windows.Forms.Button();
+            this.ComentarioTextBox = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.HabilidadComboBox = new System.Windows.Forms.ComboBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.NuevoButton = new System.Windows.Forms.Button();
+            this.GuardarButton = new System.Windows.Forms.Button();
+            this.EliminarButton = new System.Windows.Forms.Button();
+            this.ActivoCheckBox = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PasanteIdNumericUpDown)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(19, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Pasante Id";
+            // 
+            // PasanteIdNumericUpDown
+            // 
+            this.PasanteIdNumericUpDown.Location = new System.Drawing.Point(139, 13);
+            this.PasanteIdNumericUpDown.Name = "PasanteIdNumericUpDown";
+            this.PasanteIdNumericUpDown.Size = new System.Drawing.Size(150, 27);
+            this.PasanteIdNumericUpDown.TabIndex = 1;
+            // 
+            // BuscarButton
+            // 
+            this.BuscarButton.Location = new System.Drawing.Point(303, 11);
+            this.BuscarButton.Name = "BuscarButton";
+            this.BuscarButton.Size = new System.Drawing.Size(94, 29);
+            this.BuscarButton.TabIndex = 2;
+            this.BuscarButton.Text = "Buscar";
+            this.BuscarButton.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 75);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Fecha Creacion";
+            // 
+            // FechaCreacionDateTimePicker
+            // 
+            this.FechaCreacionDateTimePicker.CustomFormat = "dd/MM/yyyy";
+            this.FechaCreacionDateTimePicker.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.FechaCreacionDateTimePicker.Location = new System.Drawing.Point(140, 70);
+            this.FechaCreacionDateTimePicker.Name = "FechaCreacionDateTimePicker";
+            this.FechaCreacionDateTimePicker.Size = new System.Drawing.Size(530, 27);
+            this.FechaCreacionDateTimePicker.TabIndex = 4;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(19, 188);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(118, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nombre Pasante";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(19, 245);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(71, 20);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Matricula";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(19, 302);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Email";
+            // 
+            // EmailTextBox
+            // 
+            this.EmailTextBox.Location = new System.Drawing.Point(140, 299);
+            this.EmailTextBox.Name = "EmailTextBox";
+            this.EmailTextBox.Size = new System.Drawing.Size(530, 27);
+            this.EmailTextBox.TabIndex = 12;
+            // 
+            // InstitucionComboBox
+            // 
+            this.InstitucionComboBox.FormattingEnabled = true;
+            this.InstitucionComboBox.Location = new System.Drawing.Point(140, 127);
+            this.InstitucionComboBox.Name = "InstitucionComboBox";
+            this.InstitucionComboBox.Size = new System.Drawing.Size(530, 28);
+            this.InstitucionComboBox.TabIndex = 14;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 130);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Institucion";
+            // 
+            // MatriculaTextBox
+            // 
+            this.MatriculaTextBox.Location = new System.Drawing.Point(140, 242);
+            this.MatriculaTextBox.Name = "MatriculaTextBox";
+            this.MatriculaTextBox.Size = new System.Drawing.Size(530, 27);
+            this.MatriculaTextBox.TabIndex = 15;
+            // 
+            // NombrePasanteTextBox
+            // 
+            this.NombrePasanteTextBox.Location = new System.Drawing.Point(140, 185);
+            this.NombrePasanteTextBox.Name = "NombrePasanteTextBox";
+            this.NombrePasanteTextBox.Size = new System.Drawing.Size(530, 27);
+            this.NombrePasanteTextBox.TabIndex = 16;
+            // 
+            // TelefonoTextBox
+            // 
+            this.TelefonoTextBox.Location = new System.Drawing.Point(140, 413);
+            this.TelefonoTextBox.Name = "TelefonoTextBox";
+            this.TelefonoTextBox.Size = new System.Drawing.Size(530, 27);
+            this.TelefonoTextBox.TabIndex = 21;
+            // 
+            // HorasARealizarTextBox
+            // 
+            this.HorasARealizarTextBox.Location = new System.Drawing.Point(140, 356);
+            this.HorasARealizarTextBox.Name = "HorasARealizarTextBox";
+            this.HorasARealizarTextBox.Size = new System.Drawing.Size(530, 27);
+            this.HorasARealizarTextBox.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(19, 359);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(117, 20);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "Horas a Realizar";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(19, 416);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(67, 20);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Telefono";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RemoverButton);
+            this.groupBox1.Controls.Add(this.AgregarButton);
+            this.groupBox1.Controls.Add(this.ComentarioTextBox);
+            this.groupBox1.Controls.Add(this.label11);
+            this.groupBox1.Controls.Add(this.HabilidadComboBox);
+            this.groupBox1.Controls.Add(this.label10);
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(16, 455);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(654, 338);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Detalle de Habilidad";
+            // 
+            // RemoverButton
+            // 
+            this.RemoverButton.Location = new System.Drawing.Point(6, 301);
+            this.RemoverButton.Name = "RemoverButton";
+            this.RemoverButton.Size = new System.Drawing.Size(94, 29);
+            this.RemoverButton.TabIndex = 27;
+            this.RemoverButton.Text = "Remover";
+            this.RemoverButton.UseVisualStyleBackColor = true;
+            // 
+            // AgregarButton
+            // 
+            this.AgregarButton.Location = new System.Drawing.Point(546, 34);
+            this.AgregarButton.Name = "AgregarButton";
+            this.AgregarButton.Size = new System.Drawing.Size(98, 65);
+            this.AgregarButton.TabIndex = 26;
+            this.AgregarButton.Text = "Agregar";
+            this.AgregarButton.UseVisualStyleBackColor = true;
+            // 
+            // ComentarioTextBox
+            // 
+            this.ComentarioTextBox.Location = new System.Drawing.Point(123, 71);
+            this.ComentarioTextBox.Name = "ComentarioTextBox";
+            this.ComentarioTextBox.Size = new System.Drawing.Size(391, 27);
+            this.ComentarioTextBox.TabIndex = 25;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(17, 74);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(87, 20);
+            this.label11.TabIndex = 24;
+            this.label11.Text = "Comentario";
+            // 
+            // HabilidadComboBox
+            // 
+            this.HabilidadComboBox.FormattingEnabled = true;
+            this.HabilidadComboBox.Location = new System.Drawing.Point(123, 34);
+            this.HabilidadComboBox.Name = "HabilidadComboBox";
+            this.HabilidadComboBox.Size = new System.Drawing.Size(392, 28);
+            this.HabilidadComboBox.TabIndex = 25;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(17, 37);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(75, 20);
+            this.label10.TabIndex = 24;
+            this.label10.Text = "Habilidad";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(6, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 29;
+            this.dataGridView1.Size = new System.Drawing.Size(638, 188);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // NuevoButton
+            // 
+            this.NuevoButton.Location = new System.Drawing.Point(151, 814);
+            this.NuevoButton.Name = "NuevoButton";
+            this.NuevoButton.Size = new System.Drawing.Size(108, 39);
+            this.NuevoButton.TabIndex = 28;
+            this.NuevoButton.Text = "Nuevo";
+            this.NuevoButton.UseVisualStyleBackColor = true;
+            // 
+            // GuardarButton
+            // 
+            this.GuardarButton.Location = new System.Drawing.Point(291, 814);
+            this.GuardarButton.Name = "GuardarButton";
+            this.GuardarButton.Size = new System.Drawing.Size(108, 39);
+            this.GuardarButton.TabIndex = 29;
+            this.GuardarButton.Text = "Guardar";
+            this.GuardarButton.UseVisualStyleBackColor = true;
+            // 
+            // EliminarButton
+            // 
+            this.EliminarButton.Location = new System.Drawing.Point(431, 814);
+            this.EliminarButton.Name = "EliminarButton";
+            this.EliminarButton.Size = new System.Drawing.Size(108, 39);
+            this.EliminarButton.TabIndex = 30;
+            this.EliminarButton.Text = "Eliminar";
+            this.EliminarButton.UseVisualStyleBackColor = true;
+            // 
+            // ActivoCheckBox
+            // 
+            this.ActivoCheckBox.AutoSize = true;
+            this.ActivoCheckBox.Location = new System.Drawing.Point(597, 14);
+            this.ActivoCheckBox.Name = "ActivoCheckBox";
+            this.ActivoCheckBox.Size = new System.Drawing.Size(73, 24);
+            this.ActivoCheckBox.TabIndex = 31;
+            this.ActivoCheckBox.Text = "Activo";
+            this.ActivoCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(686, 865);
+            this.Controls.Add(this.ActivoCheckBox);
+            this.Controls.Add(this.NuevoButton);
+            this.Controls.Add(this.GuardarButton);
+            this.Controls.Add(this.EliminarButton);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.TelefonoTextBox);
+            this.Controls.Add(this.HorasARealizarTextBox);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.NombrePasanteTextBox);
+            this.Controls.Add(this.MatriculaTextBox);
+            this.Controls.Add(this.InstitucionComboBox);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.EmailTextBox);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.FechaCreacionDateTimePicker);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.BuscarButton);
+            this.Controls.Add(this.PasanteIdNumericUpDown);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Registro Pasante";
+            ((System.ComponentModel.ISupportInitialize)(this.PasanteIdNumericUpDown)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown PasanteIdNumericUpDown;
+        private System.Windows.Forms.Button BuscarButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker FechaCreacionDateTimePicker;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox EmailTextBox;
+        private System.Windows.Forms.ComboBox InstitucionComboBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox MatriculaTextBox;
+        private System.Windows.Forms.TextBox NombrePasanteTextBox;
+        private System.Windows.Forms.TextBox TelefonoTextBox;
+        private System.Windows.Forms.TextBox HorasARealizarTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button RemoverButton;
+        private System.Windows.Forms.Button AgregarButton;
+        private System.Windows.Forms.TextBox ComentarioTextBox;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox HabilidadComboBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button NuevoButton;
+        private System.Windows.Forms.Button GuardarButton;
+        private System.Windows.Forms.Button EliminarButton;
+        private System.Windows.Forms.CheckBox ActivoCheckBox;
+    }
+}
+
