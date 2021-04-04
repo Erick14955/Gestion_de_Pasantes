57c6feeb4eae943e8bda1451ff8122f6dc0d9ac5
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-04-04 19:30:48 -0400

    Programando los metodos Limpiar, LlenaClase y LlenaCampo.

--- UI/Registros/rPasantes.cs
+++ UI/Registros/rPasantes.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gestion_de_Pasantes.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -8,13 +9,55 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Gestion_de_Pasantes.UI.Registros;
+namespace Gestion_de_Pasantes.UI.Registros
 {
     public partial class rPasantes : Form
     {
         public rPasantes()
         {
             InitializeComponent();
+        }
+        private void Limpiar()
+        {
+            PasanteIdNumericUpDown.Value = 0;
+            FechaCreacionDateTimePicker.Value = DateTime.Now.Date;
+            InstitucionComboBox.ValueMember = " ";
+            NombrePasanteTextBox.Clear();
+            MatriculaTextBox.Clear();
+            EmailTextBox.Clear();
+            HorasARealizarTextBox.Clear();
+            TelefonoTextBox.Clear();
+            HabilidadComboBox.ValueMember = " ";
+            ComentarioTextBox.Clear();
+        }
+        private void LlenaCampo(Pasantes pasante)
+        {
+            PasanteIdNumericUpDown.Value = pasante.PasanteId;
+            FechaCreacionDateTimePicker.Value = pasante.FechaCreacion.Date;
+            InstitucionComboBox.Text = pasante.Institucion;
+            NombrePasanteTextBox.Text = pasante.Nombre;
+            MatriculaTextBox.Text = pasante.Matricula.ToString();
+            EmailTextBox.Text = pasante.Email;
+            HorasARealizarTextBox.Text = pasante.HorasARealizar.ToString();
+            TelefonoTextBox.Text = pasante.Telefono;
+
+            HabilidadComboBox.ValueMember = " ";
+            ComentarioTextBox.Clear();
+        }
+        private Pasantes LlenaClase()
+        {
+            Pasantes pasante = new Pasantes();
+            pasante.PasanteId = Convert.ToInt32(PasanteIdNumericUpDown.Value);
+            pasante.FechaCreacion = FechaCreacionDateTimePicker.Value.Date;
+            pasante.Nombre = NombrePasanteTextBox.Text;
+            pasante.Institucion = InstitucionComboBox.Text;
+            pasante.Matricula = int.Parse(MatriculaTextBox.Text);
+            pasante.Email = EmailTextBox.Text;
+            pasante.HorasARealizar = int.Parse(HorasARealizarTextBox.Text);
+            pasante.Telefono = TelefonoTextBox.Text;
+            
+
+            return pasante;
         }
     }
 }
