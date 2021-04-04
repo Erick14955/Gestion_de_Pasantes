6955e6b948631c829dd2bf22ed81522453aac84b
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-04-04 16:13:39 -0400

    Ajustando la ventana de registro de usuarios.

--- UI/Registros/rUsuario.Designer.cs
+++ UI/Registros/rUsuario.Designer.cs
@@ -56,7 +56,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(19, 35);
+            this.label1.Location = new System.Drawing.Point(30, 34);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(78, 20);
             this.label1.TabIndex = 0;
@@ -65,7 +65,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(19, 93);
+            this.label2.Location = new System.Drawing.Point(30, 92);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(70, 20);
             this.label2.TabIndex = 1;
@@ -74,7 +74,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(19, 151);
+            this.label3.Location = new System.Drawing.Point(30, 150);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(59, 20);
             this.label3.TabIndex = 2;
@@ -83,7 +83,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(519, 153);
+            this.label4.Location = new System.Drawing.Point(552, 151);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(46, 20);
             this.label4.TabIndex = 3;
@@ -92,7 +92,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(19, 209);
+            this.label5.Location = new System.Drawing.Point(30, 208);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(45, 20);
             this.label5.TabIndex = 4;
@@ -101,7 +101,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(254, 210);
+            this.label6.Location = new System.Drawing.Point(265, 209);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(115, 20);
             this.label6.TabIndex = 5;
@@ -109,7 +109,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(130, 30);
+            this.numericUpDown1.Location = new System.Drawing.Point(141, 29);
             this.numericUpDown1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(263, 27);
@@ -117,7 +117,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(130, 89);
+            this.textBox1.Location = new System.Drawing.Point(141, 88);
             this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(370, 27);
@@ -125,7 +125,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(130, 148);
+            this.textBox2.Location = new System.Drawing.Point(141, 147);
             this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(370, 27);
@@ -133,7 +133,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(625, 148);
+            this.textBox3.Location = new System.Drawing.Point(658, 146);
             this.textBox3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(370, 27);
@@ -141,7 +141,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(130, 207);
+            this.textBox4.Location = new System.Drawing.Point(141, 206);
             this.textBox4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox4.Name = "textBox4";
             this.textBox4.Size = new System.Drawing.Size(118, 27);
@@ -149,7 +149,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(382, 207);
+            this.textBox5.Location = new System.Drawing.Point(393, 206);
             this.textBox5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(118, 27);
@@ -158,7 +158,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(519, 210);
+            this.label7.Location = new System.Drawing.Point(552, 208);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(100, 20);
             this.label7.TabIndex = 12;
@@ -167,7 +167,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // dateTimePicker1
             // 
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
-            this.dateTimePicker1.Location = new System.Drawing.Point(625, 205);
+            this.dateTimePicker1.Location = new System.Drawing.Point(658, 203);
             this.dateTimePicker1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.Size = new System.Drawing.Size(370, 27);
@@ -175,7 +175,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // BuscarButton
             // 
-            this.BuscarButton.Location = new System.Drawing.Point(414, 27);
+            this.BuscarButton.Location = new System.Drawing.Point(425, 26);
             this.BuscarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.BuscarButton.Name = "BuscarButton";
             this.BuscarButton.Size = new System.Drawing.Size(86, 31);
@@ -185,7 +185,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // GuardarButton
             // 
-            this.GuardarButton.Location = new System.Drawing.Point(298, 289);
+            this.GuardarButton.Location = new System.Drawing.Point(316, 262);
             this.GuardarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.GuardarButton.Name = "GuardarButton";
             this.GuardarButton.Size = new System.Drawing.Size(112, 49);
@@ -195,7 +195,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // EliminarButton
             // 
-            this.EliminarButton.Location = new System.Drawing.Point(451, 289);
+            this.EliminarButton.Location = new System.Drawing.Point(469, 262);
             this.EliminarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.EliminarButton.Name = "EliminarButton";
             this.EliminarButton.Size = new System.Drawing.Size(112, 49);
@@ -205,7 +205,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // LimpiarButton
             // 
-            this.LimpiarButton.Location = new System.Drawing.Point(604, 289);
+            this.LimpiarButton.Location = new System.Drawing.Point(622, 262);
             this.LimpiarButton.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.LimpiarButton.Name = "LimpiarButton";
             this.LimpiarButton.Size = new System.Drawing.Size(112, 49);
@@ -216,7 +216,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(519, 96);
+            this.label8.Location = new System.Drawing.Point(552, 94);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(31, 20);
             this.label8.TabIndex = 21;
@@ -225,7 +225,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(625, 90);
+            this.comboBox1.Location = new System.Drawing.Point(658, 88);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(370, 28);
             this.comboBox1.TabIndex = 22;
@@ -233,7 +233,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(922, 35);
+            this.checkBox1.Location = new System.Drawing.Point(955, 33);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(73, 24);
             this.checkBox1.TabIndex = 23;
@@ -244,7 +244,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1015, 368);
+            this.ClientSize = new System.Drawing.Size(1058, 336);
             this.Controls.Add(this.checkBox1);
             this.Controls.Add(this.comboBox1);
             this.Controls.Add(this.label8);
