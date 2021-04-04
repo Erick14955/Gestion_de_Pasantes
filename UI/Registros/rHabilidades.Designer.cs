cbe3edf6f3f2b3203236de9076230e71a5f3aa6a
Anneury <Anneury@DESKTOP-AAVNCBB>
2021-04-04 17:04:57 -0400

    Ajustando y reorganizando los elementos del registro de Habilidades.

--- UI/Registros/rHabilidades.Designer.cs
+++ UI/Registros/rHabilidades.Designer.cs
@@ -46,7 +46,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(289, 247);
+            this.button4.Location = new System.Drawing.Point(306, 265);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(103, 53);
             this.button4.TabIndex = 24;
@@ -55,7 +55,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(156, 247);
+            this.button3.Location = new System.Drawing.Point(173, 262);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(103, 53);
             this.button3.TabIndex = 23;
@@ -64,7 +64,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(23, 247);
+            this.button2.Location = new System.Drawing.Point(40, 265);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(103, 53);
             this.button2.TabIndex = 22;
@@ -74,7 +74,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(15, 186);
+            this.label4.Location = new System.Drawing.Point(24, 201);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(87, 20);
             this.label4.TabIndex = 21;
@@ -82,14 +82,15 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(139, 183);
+            this.textBox1.Location = new System.Drawing.Point(148, 187);
+            this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 27);
+            this.textBox1.Size = new System.Drawing.Size(277, 48);
             this.textBox1.TabIndex = 20;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(335, 23);
+            this.button1.Location = new System.Drawing.Point(361, 23);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(64, 29);
             this.button1.TabIndex = 19;
@@ -98,18 +99,18 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // dateTimePicker1
             // 
-            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy HH:mm";
+            this.dateTimePicker1.CustomFormat = "dd/MM/yyyy";
             this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.dateTimePicker1.Location = new System.Drawing.Point(139, 78);
+            this.dateTimePicker1.Location = new System.Drawing.Point(148, 79);
             this.dateTimePicker1.Name = "dateTimePicker1";
             this.dateTimePicker1.RightToLeft = System.Windows.Forms.RightToLeft.No;
-            this.dateTimePicker1.Size = new System.Drawing.Size(260, 27);
+            this.dateTimePicker1.Size = new System.Drawing.Size(277, 27);
             this.dateTimePicker1.TabIndex = 18;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(15, 134);
+            this.label3.Location = new System.Drawing.Point(24, 134);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(64, 20);
             this.label3.TabIndex = 17;
@@ -117,22 +118,22 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(139, 131);
+            this.textBox2.Location = new System.Drawing.Point(148, 131);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(260, 27);
+            this.textBox2.Size = new System.Drawing.Size(277, 27);
             this.textBox2.TabIndex = 16;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(139, 25);
+            this.numericUpDown1.Location = new System.Drawing.Point(148, 25);
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(190, 27);
+            this.numericUpDown1.Size = new System.Drawing.Size(187, 27);
             this.numericUpDown1.TabIndex = 15;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(15, 83);
+            this.label2.Location = new System.Drawing.Point(24, 83);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 20);
             this.label2.TabIndex = 14;
@@ -141,7 +142,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(15, 27);
+            this.label1.Location = new System.Drawing.Point(24, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(92, 20);
             this.label1.TabIndex = 13;
@@ -151,7 +152,7 @@ namespace Gestion_de_Pasantes.UI.Registros
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(414, 323);
+            this.ClientSize = new System.Drawing.Size(449, 343);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
