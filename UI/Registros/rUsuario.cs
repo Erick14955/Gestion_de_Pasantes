4f23a55e6727e2bfab19377fbd401b4d88d87994
Jonathan001-cmd <77310361+Jonathan001-cmd@users.noreply.github.com>
2021-04-05 06:41:12 +0800

    Creando Metodos Limpiar, llenarClase, LlenarCampo de rUsuario

--- UI/Registros/rUsuario.cs
+++ UI/Registros/rUsuario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gestion_de_Pasantes.Entidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,40 @@ namespace Gestion_de_Pasantes.UI.Registros
         public rUsuario()
         {
             InitializeComponent();
+        }
+        private void LimpiarClase()
+        {
+            NumericUpDown1.Value = 0;
+            NombreTextBox.Clear();
+            UsuarioTextBox.Clear();
+            ClaveTextBox.Clear();
+            ConfirmarClaveTextBox.Clear();
+            RolComboBox.Text = "";
+            EmailTextBox.Clear();
+            ActivoCheckBox.Checked = false;
+        }
+        private Usuarios LlenarClase()
+        {
+            Usuarios usuario = new Usuarios();
+            usuario.UsuarioId = (int)NumericUpDown1.Value;
+            usuario.Nombre = NombreTextBox.Text;
+            usuario.Alias = UsuarioTextBox.Text;
+            usuario.Clave = ClaveTextBox.Text;
+            usuario.Rol = RolComboBox.Text;
+            usuario.Email = EmailTextBox.Text;
+            usuario.Activo = ActivoCheckBox.Checked;
+
+            return usuario;
+        }
+        private void LlenarCampo(Usuarios usuarios)
+        {
+            NumericUpDown1.Value = usuarios.UsuarioId;
+            NombreTextBox.Text = usuarios.Nombre;
+            UsuarioTextBox.Text = usuarios.Alias;
+            ClaveTextBox.Text = usuarios.Clave;
+            RolComboBox.Text = usuarios.Rol;
+            EmailTextBox.Text = usuarios.Email;
+            ActivoCheckBox.Checked = usuarios.Activo;
         }
     }
 }
